234204a8aefe0478f33514be23e67d57fc954ddf
HuemanTarget <38532350+HuemanTarget@users.noreply.github.com>
2020-01-21 19:30:43 -0800

    add comment function to show page

--- brag-board/routes/comments.js
+++ brag-board/routes/comments.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var commentsCtrl = require('../controllers/comments');
 
+/* router.get('/', (req,res)=>{
+    res.redirect('/games')
+}) */
+
 router.post('/games/:id/comments', commentsCtrl.create);
 
 module.exports = router;
--- brag-board/routes/games.js
+++ brag-board/routes/games.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var gamesCtrl = require("../controllers/games");
+var commentsCtrl = require('../controllers/comments');
 
 router.get("/", gamesCtrl.index);
 router.get("/new", gamesCtrl.new);
@@ -8,6 +9,7 @@ router.get("/:id", gamesCtrl.show);
 router.get('/edit/:id', gamesCtrl.edit)
 router.put('/edit/:id', gamesCtrl.update)
 router.post("/", gamesCtrl.create);
+router.post('/:id/comments', commentsCtrl.create);
 router.delete('/:id', gamesCtrl.delete);
 
 module.exports = router;
